c24696ebdf485fd13d5e4566dc44ae3e9785a636
SeungkeyMouse <ksg19980@gmail.com>
2022-01-13 15:42:54 +0900

    싱글톤- @Configuration 있어야 싱글톤이 유지됨

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -18,16 +18,19 @@ public class AppConfig {//전체 구성: 공연기획자가 넣어줌
 
     @Bean
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
